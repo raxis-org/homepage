21acb4f69e9737e4697ca0679210a0046040875a
yamaguchi <aidukaibuki810@gmail.com>
2025-02-22 16:16:11 +0900

    header,footer,mainpageの修正

--- raxis-tech-homepage/src/app/components/Header.tsx
+++ raxis-tech-homepage/src/app/components/Header.tsx
@@ -1,47 +1,50 @@
 // ヘッダーコンポーネント
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Header() {
     return (
         <header className="z-10 sticky bottom-0 left-0 right-0 top-4 h-[80px] w-[100vw] m-auto rounded-full bg-white shadow-lg lg:w-[1024px] px-8 overflow-hidden">
-            <div id="header-inner" className="w-full m-auto flex flex-nowrap justify-between items-center text-center  ">
+            <div id="header-inner" className="w-full h-full m-auto flex flex-nowrap justify-between items-center text-center  ">
                 <div id="header-icon-wrapper" className="flex flex-nowrap">
-                <Image
-                    className="dark:invert"
-                    src="/assets/icon/icon-with-text.png"
-                    alt="Site logo"
-                    width={250}
-                    height={20}
-                    priority
-                />
+                    <Link href="/">
+                        <Image
+                            className="dark:invert"
+                            src="/assets/icon/icon-with-text.png"
+                            alt="Site logo"
+                            width={250}
+                            height={20}
+                            priority
+                        />
+                    </Link>
                 <h1 className="font-sans font-bold text-[2rem] text-blue-700"></h1>
                 
                 </div>
-                <div id="header-contents-wrapper" className="flex flex-nowrap justify-between">
-                    <a className="header-content" href="#">
+                <div id="header-contents-wrapper" className="hidden md:flex flex-nowrap justify-between h-full">
+                    <Link className="header-content" href="#">
                         <div className="header-content-en">ABOUT</div>
                         <div className="header-content-label">Raxisとは</div>
-                    </a>
+                    </Link>
                     
-                    <a className="header-content" href="#">
+                    <Link className="header-content" href="/about">
                         <div className="header-content-en">PRODUCTS</div>
                         <div className="header-content-label">事業内容</div>
-                    </a>
+                    </Link>
                     
-                    <a className="header-content" href="#">
+                    <Link className="header-content" href="#">
                         <div className="header-content-en">WORKS</div>
                         <div className="header-content-label">ポートフォリオ</div>
-                    </a>
+                    </Link>
                     
-                    <a className="header-content" href="#">
+                    <Link className="header-content" href="#">
                         <div className="header-content-en">BLOG</div>
                         <div className="header-content-label">ブログ</div>
-                    </a>
+                    </Link>
                     
-                    <a className="header-content" href="#">
+                    <Link className="header-content" href="#">
                         <div className="header-content-en">CONTACT</div>
                         <div className="header-content-label">お問い合わせ</div>
-                    </a>
+                    </Link>
                 </div>
             </div>
         </header>
--- raxis-tech-homepage/src/app/layout.tsx
+++ raxis-tech-homepage/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import "./header.css";
+import "./footer.css";
 import "./page.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
