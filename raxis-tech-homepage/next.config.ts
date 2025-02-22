c5baf85697e409ba446dee481cb37c25d6301c2b
yamaguchi <aidukaibuki810@gmail.com>
2025-02-22 21:48:08 +0900

    next.configをいじり、本番環境で画像が表示されるように

--- raxis-tech-homepage/next.config.ts
+++ raxis-tech-homepage/next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export", // 静的エクスポートを有効にする
+  domains: ["https://raxistech.netlify.app","https://raxis.tech"]
 };
 
 export default nextConfig;
