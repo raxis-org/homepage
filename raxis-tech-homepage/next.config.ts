54cf24783daa8d8bddf554b9bfb6a65e71c09907
yamaguchi <aidukaibuki810@gmail.com>
2025-02-22 22:04:31 +0900

    画像が表示されないバグへのタックル5

--- raxis-tech-homepage/next.config.ts
+++ raxis-tech-homepage/next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "standalone",
+  output: "export",
   images:{
     loader: "default",
     domains: ["raxistech.netlify.app","https://raxis.tech"],
