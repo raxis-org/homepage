cdf3fd17d2981a764c5c19e10f4dc8655e49c072
yamaguchi <aidukaibuki810@gmail.com>
2025-03-02 13:38:52 +0900

    モバイル版のheaderのデザイン修正

--- raxis-tech-homepage/src/app/components/Header.tsx
+++ raxis-tech-homepage/src/app/components/Header.tsx
@@ -9,10 +9,10 @@ export default function Header() {
                 <div id="header-icon-wrapper" className="flex flex-nowrap">
                     <Link href="/">
                         <Image
-                            className="dark:invert"
+                            className="w-[200px]"
                             src="/assets/icon/icon-with-text.png"
                             alt="Site logo"
-                            width={250}
+                            width={200}
                             height={20}
                             priority
                         />
@@ -45,6 +45,18 @@ export default function Header() {
                         <div className="header-content-en">CONTACT</div>
                         <div className="header-content-label">お問い合わせ</div>
                     </Link>
+                </div>
+                <div className="sm:hidden sm:shadow-[0 0 0 2000px #00000090] block fixed m-auto top-0 left-0 right-0 bottom-0 w-[calc(90vw-4rem)] h-[90vh] bg-white shadow-2xl">
+                    <input type="checkbox" className="menu-btn hidden" id="menu-btn"/>
+                    <label htmlFor="menu-btn" className="menu-icon"><span className="navicon"></span></label>
+                    <div className="flex flex-col justify-start align-center" id="header-menu-ls">
+                        <div className="top"><div className="header-menu-en">HOME</div><Link href="/">ホーム</Link></div>
+                        <div><Link href="/Linkbout"><div className="header-menu-en">ABOUT</div>Raxisとは</Link></div>
+                        <div><Link href="/products"><div className="header-menu-en">PRODUCTS</div>事業内容</Link></div>
+                        <div><Link href="/works"><div className="header-menu-en">WORKS</div>ポートフォリオ</Link></div>
+                        <div><Link href="/blog"><div className="header-menu-en">BLOG</div>ブログ</Link></div>
+                        <div><Link href="/contLinkct"><div className="header-menu-en">CONTACT</div>お問い合わせ</Link></div>
+                    </div>
                 </div>
             </div>
         </header>
